d321ea6163af4e1cc95db649aaa9de6e55226ff5
imshoppinginwalmart <dogslovetobark@gmail.com>
2025-05-09 15:55:29 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,9 +2,9 @@ local owner = 'imshoppinginwalmart'
 local branch = 'main'
 
 local function webImport(file)
-    return loadstring(game:HttpGetAsync(('https://raw.githubusercontent.com/%s/Library/%s/%s.lua'):format(owner, branch, file)), file .. '.lua')()
+    return loadstring(game:HttpGetAsync(('https://raw.githubusercontent.com/%s/Library/%s/%s'):format(owner, branch, file)), file .. '.lua')()
 end
 
 _G.webImport = webImport
 
-return webImport('Core')
+return webImport('Core.lua')
