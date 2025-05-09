d16321b524828811e08c7d40548aaab365f449be
imshoppinginwalmart <dogslovetobark@gmail.com>
2025-05-09 15:55:08 -0400

    Update Button.lua

--- Components/Button.lua
+++ Components/Button.lua
@@ -1,1 +1,19 @@
+local Component = _G.GUI_webImport("Component.lua")
+local Button    = setmetatable({}, Component)
+Button.__index  = Button
+Button.__className = "Button"
 
+function Button:_createInstance()
+    local btn = Instance.new("TextButton")
+    btn.Size  = self.props.size or UDim2.new(0,120,0,32)
+    btn.Text  = self.props.text or "Button"
+    btn.Parent = self.props.parent
+
+    btn.MouseButton1Click:Connect(function()
+        if self.props.onClick then self.props.onClick() end
+    end)
+
+    self._inst = btn
+end
+
+return Button
